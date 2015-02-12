5e31143656469b587f5e721e88c82754529b9e44
Yuichi Shiwaku <yuichi.shiwaku@gmail.com>
2015-02-12 17:09:09 +0900

    Fix format.

--- incoming/incoming.go
+++ incoming/incoming.go
@@ -1,51 +1,51 @@
 package incoming
 
 import (
-	"encoding/json"
-	"net/http"
-	"net/url"
+    "encoding/json"
+    "net/http"
+    "net/url"
 )
 
 // Config struct
 type Config struct {
-	WebHookURL string
+    WebHookURL string
 }
 
 // https://api.slack.com/docs/attachments
 
 // Field struct
 type Field struct {
-	Title string `json:"title"`
-	Value string `json:"value"`
-	Short bool   `json:"short"`
+    Title string `json:"title"`
+    Value string `json:"value"`
+    Short bool   `json:"short"`
 }
 
 // Attachment struct
 type Attachment struct {
-	Fallback string  `json:"fallback"`
-	Pretext  string  `json:"pretext"`
-	Color    string  `json:"color"`
-	Fields   []Field `json:"fields"`
+    Fallback string  `json:"fallback"`
+    Pretext  string  `json:"pretext"`
+    Color    string  `json:"color"`
+    Fields   []Field `json:"fields"`
 }
 
 // Payload struct
 type Payload struct {
-	Attachments []Attachment `json:"attachments"`
+    Attachments []Attachment `json:"attachments"`
 }
 
 // Post method
 func Post(conf Config, payload Payload) error {
-	p, err := json.Marshal(&payload)
-	if err != nil {
-		return err
-	}
+    p, err := json.Marshal(&payload)
+    if err != nil {
+        return err
+    }
 
-	_, err = http.PostForm(conf.WebHookURL, url.Values{
-		"payload": []string{string(p)},
-	})
+    _, err = http.PostForm(conf.WebHookURL, url.Values{
+        "payload": []string{string(p)},
+    })
 
-	if err != nil {
-		return err
-	}
-	return nil
+    if err != nil {
+        return err
+    }
+    return nil
 }
